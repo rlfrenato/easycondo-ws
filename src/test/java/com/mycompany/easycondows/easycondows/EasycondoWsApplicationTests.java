2bf90c8f0c9a4661bff393919351aadd1d485788
Renato Franceschinelli <rlf.renato@gmail.com>
2018-02-19 21:57:42 -0300

    EC-1: Initial project setup

---
+++ src/test/java/com/mycompany/easycondows/easycondows/EasycondoWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mycompany.easycondows.easycondows;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasycondoWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
